a5cafca030ac484fbfd3dd2fda22072944e56c65
Aleksandr <popkov.aleks.v@yandex.ru>
2022-05-23 16:59:03 +0500

    Логирование

--- 01_docker_compose/app/config/settings.py
+++ 01_docker_compose/app/config/settings.py
@@ -23,3 +23,32 @@ include(
     'components/default_pk_field_type.py',
     'components/allowed_hosts.py'
 )
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': True,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'formatters': {
+        'default': {
+            'format': '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]',
+        },
+    },
+    'handlers': {
+        'debug-console': {
+            'class': 'logging.StreamHandler',
+            'formatter': 'default',
+            'filters': ['require_debug_true'],
+        },
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['debug-console'],
+            'propagate': False,
+        }
+    },
+}
