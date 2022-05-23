a61a516f0a69e32f027a3a46bd6ba79a198d6201
Aleksandr <popkov.aleks.v@yandex.ru>
2022-05-23 17:00:36 +0500

    Хосты для Debug toolbar

--- 01_docker_compose/app/config/components/allowed_hosts.py
+++ 01_docker_compose/app/config/components/allowed_hosts.py
@@ -1,5 +1,7 @@
 ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0']
 
-INTERNAL_IPS = [
-    "127.0.0.1",
-]
+
+if DEBUG:
+    import socket  # only if you haven't already imported this
+    hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
+    INTERNAL_IPS = [ip[: ip.rfind(".")] + ".1" for ip in ips] + ["127.0.0.1", "10.0.2.2"]
