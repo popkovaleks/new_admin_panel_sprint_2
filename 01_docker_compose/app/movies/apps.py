30116eb2333055cbb73ab10f2674872bbb3f24e4
Aleksandr <popkov.aleks.v@yandex.ru>
2022-05-23 17:00:13 +0500

    Сигналы

--- 01_docker_compose/app/movies/apps.py
+++ 01_docker_compose/app/movies/apps.py
@@ -6,3 +6,6 @@ class MoviesConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'movies'
     verbose_name = _('movies')
+
+    def ready(self):
+        import movies.signals
